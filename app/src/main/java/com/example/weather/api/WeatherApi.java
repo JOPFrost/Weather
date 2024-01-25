4dee0c5187a3c44f64b672e9fa6c41247e1c0e33
frost <frizii@email.cz>
2024-01-25 14:21:11 +0100

    add weather api interface (calling weather)

---
+++ app/src/main/java/com/example/weather/api/WeatherApi.java
@@ -0,0 +1,12 @@
+package com.example.weather.api;
+
+import com.example.weather.model.WeatherResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherApi {
+    @GET("/v1/current.json")
+    Call<WeatherResponse> getCurrent(@Query("key") String key,
+                                     @Query("city") String city);
+}
